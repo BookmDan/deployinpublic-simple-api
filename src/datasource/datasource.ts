34a0e3ec32461089db4fb5779ae1a6982cd6b21b
bookmdan <danoh329@gmail.com>
2024-07-27 21:04:31 -0700

    commit changes

--- src/datasource/datasource.ts
+++ src/datasource/datasource.ts
@@ -2,23 +2,16 @@ import { DataSource } from 'typeorm';
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: 'postgres',
+  database: process.env.DB_NAME,
   logging: true,
   synchronize: false,
-  ssl: process.env.ENVIRONMENT === 'production' ? true : false,
-  extra:
-    process.env.ENVIRONMENT === 'production'
-      ? {
-          ssl: {
-            rejectUnauthorized:
-              process.env.ENVIRONMENT === 'production' ? false : true,
-          },
-        }
-      : null,
+  ssl: process.env.ENVIRONMENT === 'production' ? {
+    rejectUnauthorized: false
+  } : undefined,
   entities: [`${__dirname}/../**/**.entity{.ts,.js}`],
   migrations: [`${__dirname}/../migrations/*{.ts,.js}`],
 });
